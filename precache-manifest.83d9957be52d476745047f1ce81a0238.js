65c37c78a0b801083dba1da5980f888d46d13a72
MarMMcin <em-erce@wp.pl>
2020-02-07 02:11:49 +0100

    Updates

---
+++ precache-manifest.83d9957be52d476745047f1ce81a0238.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f306d2dce315c6de8f90e44aeceaa60c",
+    "url": "/rzeszowczoraj/index.html"
+  },
+  {
+    "revision": "2edd2e4c520bac367d41",
+    "url": "/rzeszowczoraj/static/css/main.b13d2f5e.chunk.css"
+  },
+  {
+    "revision": "4476983cbbf286044937",
+    "url": "/rzeszowczoraj/static/js/2.c2287c10.chunk.js"
+  },
+  {
+    "revision": "bacbe4135fbff86e841bcef3717677d8",
+    "url": "/rzeszowczoraj/static/js/2.c2287c10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2edd2e4c520bac367d41",
+    "url": "/rzeszowczoraj/static/js/main.6f8775ae.chunk.js"
+  },
+  {
+    "revision": "87051151cf79ca1bcd70",
+    "url": "/rzeszowczoraj/static/js/runtime-main.f33948fd.js"
+  },
+  {
+    "revision": "1124aa58ac9f2c8bc736d645a25e21f4",
+    "url": "/rzeszowczoraj/static/media/fara1after.1124aa58.png"
+  },
+  {
+    "revision": "b5acddf59757aad58cbedafe13d2423a",
+    "url": "/rzeszowczoraj/static/media/fara1before.b5acddf5.png"
+  },
+  {
+    "revision": "bbec255fbe6ae8d789c565a958322c8d",
+    "url": "/rzeszowczoraj/static/media/logomapa.bbec255f.jpg"
+  },
+  {
+    "revision": "a421f16c16bcc886e78e4feff50fea47",
+    "url": "/rzeszowczoraj/static/media/mick1after.a421f16c.png"
+  },
+  {
+    "revision": "43603e3dfb29b714ff130f2c3f69cb6a",
+    "url": "/rzeszowczoraj/static/media/mick1before.43603e3d.png"
+  },
+  {
+    "revision": "1c3b553a34e3e15c432df277b8b20e2b",
+    "url": "/rzeszowczoraj/static/media/plac1after.1c3b553a.png"
+  },
+  {
+    "revision": "c7c82792b6af53a06425f0dcff28131d",
+    "url": "/rzeszowczoraj/static/media/plac1before.c7c82792.png"
+  },
+  {
+    "revision": "ebf283df651027d179cde2a4b93b98b6",
+    "url": "/rzeszowczoraj/static/media/poczt1after.ebf283df.png"
+  },
+  {
+    "revision": "250cbba249eaa57de996ce6543adec90",
+    "url": "/rzeszowczoraj/static/media/poczt1before.250cbba2.png"
+  },
+  {
+    "revision": "b2e3a5a3ed645eee511f5c963a37eabf",
+    "url": "/rzeszowczoraj/static/media/rynek1after.b2e3a5a3.png"
+  },
+  {
+    "revision": "a3007aa2de3907b97d760e20884641cc",
+    "url": "/rzeszowczoraj/static/media/rynek1before.a3007aa2.png"
+  },
+  {
+    "revision": "849c64e7b77c5aa64de43f3c57116b53",
+    "url": "/rzeszowczoraj/static/media/zamk1after.849c64e7.png"
+  },
+  {
+    "revision": "98f95d5d69e0d7e55e308bbe7df00f11",
+    "url": "/rzeszowczoraj/static/media/zamk1before.98f95d5d.png"
+  },
+  {
+    "revision": "a111e95e67818a0ef7b07693f69372ff",
+    "url": "/rzeszowczoraj/static/media/zygm1after.a111e95e.png"
+  },
+  {
+    "revision": "62570428ecfd9517354632ae1a0167df",
+    "url": "/rzeszowczoraj/static/media/zygm1before.62570428.png"
+  }
+]);
